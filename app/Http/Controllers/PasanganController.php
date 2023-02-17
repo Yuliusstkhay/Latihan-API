f42676fb84c0b1fbb074216c6498bbe287b28e37
Yulius <1stYesus@gmail.com>
2023-02-17 22:48:15 +0700

    Commit Pertama

---
+++ app/Http/Controllers/PasanganController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class PasanganController extends Controller
+{
+    function index(){
+        $json=['Yus'=>'Chika'];
+        $client=new Client();
+        $hasil= $client->request('POST','http://localhost:10000',['json'=>$json])->getBody();
+        // print_r(json_decode($hasil->getContents()));
+        return $hasil;
+    }
+}
