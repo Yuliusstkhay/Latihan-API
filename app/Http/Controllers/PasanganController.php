a116d8fefc85bef8ce36c4abde70f3a312b8a402
Yulius <1stYesus@gmail.com>
2023-02-17 23:51:49 +0700

    Perbaikan app ke-1

--- app/Http/Controllers/PasanganController.php
+++ app/Http/Controllers/PasanganController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PasanganController extends Controller
 {
     function index(){
-        $json=['Yus'=>'Chika'];
+        $json=['pria'=>'Yulius','wanita'=>'Yessica'];
         $client=new Client();
         $hasil= $client->request('POST','http://localhost:10000',['json'=>$json])->getBody();
         // print_r(json_decode($hasil->getContents()));
